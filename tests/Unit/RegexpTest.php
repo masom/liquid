7aa292c903bc004fb73045c9d99a421af90c3bb6
Martin Samson <martin@canvaspop.com>
2014-07-01 16:43:15 -0400

    Regexp tests.

--- tests/Unit/RegexpTest.php
+++ tests/Unit/RegexpTest.php
@@ -32,4 +32,36 @@ class RegexpTest extends \Liquid\Tests\TestCase {
         $this->assertEquals(3, preg_match_all(Liquid::$QuotedFragment, '<style class="hello">\' </style>', $matches));
         $this->assertEquals(array('<style', 'class="hello">', '</style>'), $matches[0]);
     }
+
+    public function test_double_quoted_words() {
+        $this->assertEquals(3, preg_match_all(Liquid::$QuotedFragment, 'arg1 arg2 "arg 3"', $matches));
+        $this->assertEquals(array('arg1', 'arg2', '"arg 3"'), $matches[0]);
+    }
+
+    public function test_single_quoted_words() {
+        $this->assertEquals(3, preg_match_all(Liquid::$QuotedFragment, "arg1 arg2 'arg 3'", $matches));
+        $this->assertEquals(array('arg1', 'arg2', "'arg 3'"), $matches[0]);
+    }
+
+    public function test_quoted_words_in_the_middle() {
+        $this->assertEquals(4, preg_match_all(Liquid::$QuotedFragment, 'arg1 arg2 "arg 3" arg4   ', $matches));
+        $this->assertEquals(['arg1', 'arg2', '"arg 3"', 'arg4'], $matches[0]);
+    }
+
+    public function test_variable_parser() {
+        $this->assertEquals(1, preg_match_all(Liquid::$VariableParser, 'var', $matches));
+        $this->assertEquals(array('var'), $matches[0]);
+
+        $this->assertEquals(2, preg_match_all(Liquid::$VariableParser, 'var.method', $matches));
+        $this->assertEquals(array('var', 'method'), $matches[0]);
+
+        $this->assertEquals(2, preg_match_all(Liquid::$VariableParser, 'var[method]', $matches));
+        $this->assertEquals(array('var', '[method]'), $matches[0]);
+
+        $this->assertEquals(3, preg_match_all(Liquid::$VariableParser, 'var[method][0]', $matches));
+        $this->assertEquals(array('var', '[method]', '[0]'), $matches[0]);
+
+        $this->assertEquals(4, preg_match_all(Liquid::$VariableParser, 'var[method][0].method', $matches));
+        $this->assertEquals(array('var', '[method]', '[0]', 'method'), $matches[0]);
+    }
 }
