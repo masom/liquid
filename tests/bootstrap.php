effcc22f27362e10e4f59513c4921b08fe153b9d
Martin Samson <martin@canvaspop.com>
2014-07-06 11:31:17 -0400

    Added error mode test configuration.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,3 +5,11 @@ date_default_timezone_set('UTC');
 $loader = require __DIR__ . '/../vendor/autoload.php';
 $loader->addPsr4('Liquid\\Tests\\', __DIR__);
 
+
+$mode = \Liquid\Liquid::ERROR_MODE_STRICT;
+
+if ($env_mode = getenv('LIQUID_PARSER_MODE')) {
+    echo '-- ' . strtoupper($env_mode) . " ERROR MODE \n";
+    $mode = $env_mode;
+}
+\Liquid\Template::error_mode($mode);
