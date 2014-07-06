e02207778dcf01c81ea8e29454e3f2838f0c4dd3
Martin Samson <martin@canvaspop.com>
2014-07-06 11:39:23 -0400

    Added some variable tests to help pintpoint scope problems.

---
+++ tests/Integration/VariableTest.php
@@ -0,0 +1,33 @@
+<?php
+namespace Liquid\Tests\Integration;
+
+use \Liquid\Template;
+
+class VariableTest extends \Liquid\Tests\TestCase {
+
+    public function test_simple_variable() {
+        $template = Template::parse('{{test}}');
+        $this->assertEquals('worked', $template->render(array('test'=>'worked')));
+        $this->assertEquals('worked wonderfully', $template->render(array('test'=>'worked wonderfully')));
+    }
+
+    public function test_simple_with_whitespaces() {
+        $template = Template::parse('  {{test}}  ');
+        $this->assertEquals('  worked  ', $template->render(array('test'=>'worked')));
+        $this->assertEquals('  worked wonderfully  ', $template->render(array('test'=>'worked wonderfully')));
+    }
+
+    public function test_ignore_unknown() {
+        $template = Template::parse('{{ test }}');
+        $this->assertEquals('', $template->render());
+    }
+
+    public function test_hash_scoping() {
+        $template = Template::parse('{{ test.test }}');
+        $this->assertEquals('worked', $template->render(array('test' => array('test' => 'worked'))));
+    }
+
+    public function test_false_renders_as_false() {
+        $this->assertEquals('false', Template::parse('{{ foo }}')->render(array('foo' => false)));
+    }
+}
